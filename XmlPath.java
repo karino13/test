c8a45ce5c6597f2a3592669c5ae3beda06fdf11c
karino13 <49114987+karino13@users.noreply.github.com>
2019-06-04 05:01:07 +0900

    Create XmlPath.java

---
+++ XmlPath.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+//jar파일 압축 풀기
+class Jar {
+
+    public static String XmlFunc(String args) {
+        String[] cmd = { "cmd", "/c",  "cd c:\\ && apktool d -f InsecureBankv2.apk"};
+        Process process = null;
+        try {
+            process = new ProcessBuilder(cmd).start();
+
+            Scanner s = new Scanner(process.getInputStream(), "EUC-KR");
+            while (s.hasNextLine() == true) {
+                System.out.println(s.nextLine());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return"c:"+args+"\\AndroidManifest.xml"; //xml경로를 return
+    }
+
+}
+public abstract class Main{
+    public static String FileName = "InsecureBankv2.apk";
+    public static void main(String[] args){
+        String XmlPath;
+        XmlPath = Jar.XmlFunc(FileName);
+        System.out.println(XmlPath);
+    }
+}
