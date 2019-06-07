c80761a4dd56ed0e4a63b06812938cb5dd0c3add
karino13 <49114987+karino13@users.noreply.github.com>
2019-06-08 05:13:27 +0900

    Update ClassList.java

--- ClassList.java
+++ ClassList.java
@@ -12,7 +12,7 @@ class ClassList {
     public static String[] filelist = new String[9999];
     public static String[] Start() {
         try {
-            String path="C:\\test1";
+            String path="C:\\test1"; // --------------- 읽어들일 DIR 경로 
             System.out.println("Directory Name:" + path);
             // main process.
             (new ClassList()).showFileList(path);
@@ -29,6 +29,7 @@ class ClassList {
 
             if (file.isFile()) {
                 if(file.getCanonicalPath().toString().contains(".class")) filelist[j] = file.getCanonicalPath().toString(); System.out.println(j+filelist[j]);j++;
+            //파일 목록 출력 없애려면 바로 윗줄의 System.out.println(j+filelist[j]); 부분을 지우시면 됩니다
             } else if (file.isDirectory()) { try { showFileList(file.getCanonicalPath().toString()); } catch (Exception e) { } }
         }
     }
